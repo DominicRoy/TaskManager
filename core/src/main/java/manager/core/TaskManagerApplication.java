2a8fb8e6a87f341dd4102f4a6ba47decc0931d7d
Dico <dom.roy2@gmail.com>
2018-08-12 14:46:39 -0400

    Simple UI and CSV recording

--- core/src/main/java/manager/core/TaskManagerApplication.java
+++ core/src/main/java/manager/core/TaskManagerApplication.java
@@ -1,32 +1,55 @@
 package manager.core;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import manager.core.csv.CSVReader;
 import manager.core.tasks.Task;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
-public class TaskManagerApplication
+public class TaskManagerApplication extends Application
 {
+    
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/TaskManagerMain.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    @Override
+    public void stop() throws Exception
+    {
+        System.exit(0);
+    }
     
     public static void main(String[] args)
     {
-        CSVReader reader = new CSVReader();
-        try
-        {
-            
-            List<Task> tasks = reader.readCsv("C:\\Users\\Dico\\IdeaProjects\\TaskManager\\TaskStorage\\PrototypeStorageFile.csv");
-            tasks.forEach(task -> {
-                System.out.println(
-                        task.getTaskTitle() + task.getPriority() + task.getDescription() + task.getCreationDate() +
-                        task.getDoBeforeDate());
-            });
-            reader.addTaskEntry("C:\\Users\\Dico\\IdeaProjects\\TaskManager\\TaskStorage\\PrototypeStorageFile.csv", tasks.get(0));
-        } catch (IOException e)
-        {
-            System.out.println("Try again");
-            e.printStackTrace();
-        }
+        launch(args);
+        
+//        CSVReader reader = new CSVReader();
+//        try
+//        {
+//
+//            List<Task> tasks = reader.readCsv("C:\\Users\\Dico\\IdeaProjects\\TaskManager\\TaskStorage\\PrototypeStorageFile.csv");
+//            tasks.forEach(task -> {
+//                System.out.println(
+//                        task.getTaskTitle() + task.getPriority() + task.getDescription() + task.getCreationDate() +
+//                        task.getDoBeforeDate());
+//            });
+//            reader.addTaskEntry("C:\\Users\\Dico\\IdeaProjects\\TaskManager\\TaskStorage\\PrototypeStorageFile.csv", tasks.get(0));
+//        } catch (IOException e)
+//        {
+//            System.out.println("Try again");
+//            e.printStackTrace();
+//        }
     }
     
 }
---
+++ core/src/main/java/manager/gui/TaskManagerController.java
@@ -0,0 +1,76 @@
+package manager.gui;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import manager.core.csv.CSVReader;
+import manager.core.tasks.Task;
+import manager.core.tasks.TaskPriority;
+import manager.core.util.TimeConverter;
+
+import java.io.IOException;
+
+public class TaskManagerController implements TimeConverter
+{
+    
+    @FXML
+    private ListView<String> taskList;
+    
+    @FXML
+    private Button addEntryButton, displayTasksButton;
+    
+    @FXML
+    private TextField startDateField, endDateField, titleField, priorityField, descriptionField;
+    
+    private CSVReader reader = new CSVReader();
+    
+    @FXML
+    private void initialize()
+    {
+        addEntryButton.setOnAction(action -> {
+            try
+            {
+                reader.addTaskEntry("C:\\Users\\Dico\\IdeaProjects\\TaskManager\\TaskStorage\\PrototypeStorageFile.csv",
+                                    new Task.TaskBuilder(stringToTimestamp(startDateField.getText()),
+                                                         stringToTimestamp(endDateField.getText()),
+                                                         TaskPriority.valueOf(priorityField.getText()),
+                                                         titleField.getText(),
+                                                         descriptionField.getText()).build());
+                
+                refreshTaskList();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        });
+        
+        refreshTaskList();
+    }
+    
+    @FXML
+    private void refreshTaskList()
+    {
+        Platform.runLater(() -> {
+            taskList.getItems().setAll(FXCollections.observableArrayList());
+            
+            ObservableList<String> observableStrings = FXCollections.observableArrayList();
+            try
+            {
+                reader.readCsv("C:\\Users\\Dico\\IdeaProjects\\TaskManager\\TaskStorage\\PrototypeStorageFile.csv")
+                      .forEach(task -> {
+                          observableStrings.add(
+                                  "Title: " + task.getTaskTitle() + "Start Time: " + task.getCreationDate());
+                      });
+                taskList.getItems().setAll(observableStrings);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            
+        });
+    }
+}
